394381373323c133778c0f4821c1fe48031a7f8e
chinmayparekh <chinmayparekh11@gmail.com>
2022-07-23 18:16:39 +0530

    Modals created with validation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Page1.css";
-
-import {Card,CardImg, CardBody, CardHeader,CardText} from 'reactstrap';
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+} from "mdb-react-ui-kit";
+import { Card, CardImg, CardBody, CardHeader, CardText } from "reactstrap";
 
 const Layout = (props) => {
+  const [basicModal, setBasicModal] = useState(false);
+  const toggleShow = () => {
+    setBasicModal(!basicModal);
+  };
+  const [name, setName] = useState("");
+  const [license, setLicense] = useState("");
+  const [email, setEmail] = useState("");
+
+  const textStyle = {
+    border: "2px solid red",
+  };
+  const hide = {
+    display: "none",
+  };
+  const checkForm = (data) => {
+    //form is valid
+    if (data.name !== "" && data.license !== "" && data.email !== "") {
+      console.log("Valid");
+      textStyle.border = "2px solid green";
+      toggleShow();
+      props.register();
+    }
+    //form is invalid
+    else {
+      console.log("Invalid");
+      hide.display = "inline";
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, license, email };
+    checkForm(data);
+    console.log(data);
+  };
+
   return (
-      <Card className="text-center">
-        <CardHeader color="dark" className="layout-heading">{props.title}</CardHeader>
-        <CardImg top src={props.image} alt={props.id} className="layout-image"></CardImg>
-        <CardBody>
-          <CardText>{props.content}</CardText>
-          <button className="btn btn-info" onClick={props.register}>Register as {props.id}</button>
-        </CardBody>
-      </Card>
-  )  
+    <Card className="text-center">
+      <CardHeader color="dark" className="layout-heading">
+        {props.title}
+      </CardHeader>
+      <CardImg
+        top
+        src={props.image}
+        alt={props.id}
+        className="layout-image"
+      ></CardImg>
+      <CardBody>
+        <CardText>{props.content}</CardText>
+        <button className="save" onClick={toggleShow}>
+          Register
+        </button>
+        <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
+          <MDBModalDialog>
+            <MDBModalContent>
+              <MDBModalHeader>
+                <MDBModalTitle>
+                  <p className="title">{props.modalTitle}</p>
+                </MDBModalTitle>
+                <MDBBtn
+                  className="btn-close"
+                  color="none"
+                  onClick={toggleShow}
+                ></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>
+                <div className="register">
+                  <form onSubmit={handleSubmit}>
+                    <label>{props.modal1} </label>
+                    <input
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                    <label>{props.modal2} </label>
+                    <input
+                      type="text"
+                      value={license}
+                      onChange={(e) => setLicense(e.target.value)}
+                      required
+                    />
+                    <label>{props.modal3} </label>
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </form>
+                </div>
+              </MDBModalBody>
+
+              <MDBModalFooter>
+                <button onClick={toggleShow} className="save">
+                  Close
+                </button>
+                <button onClick={handleSubmit} className="save">
+                  Save Changes
+                </button>
+              </MDBModalFooter>
+            </MDBModalContent>
+          </MDBModalDialog>
+        </MDBModal>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Layout;
--- src/components/pages/Page2.js
+++ src/components/pages/Page2.js
@@ -1,100 +1,148 @@
-import React from "react";
 import "../../styles/Page1.css";
 import Layout from "../Layout";
-import { registerDonor,registerNGO,registerStore } from "../../utils/operation";
+import {
+  registerDonor,
+  registerNGO,
+  registerStore,
+} from "../../utils/operation";
 import { useNavigate } from "react-router-dom";
 import ngo from "../images/ngo.png";
 import donors from "../images/donor.png";
 import happy from "../images/happy.png";
 import store from "../images/store.png";
 import help from "../images/help.png";
-import {Card, CardBody,CardText, CardImg, CardHeader} from 'reactstrap';
+import { Card, CardBody, CardText, CardImg, CardHeader } from "reactstrap";
+
 
 const Page2 = (props) => {
   let navigate = useNavigate();
   const routeChange = (path) => {
     navigate(path);
   };
+
   return (
     <div id="Page2">
-        <div className="row h-500 py-5 info-row">
-          <div className="col-12 col-sm-6">
-          <Card  color="success" inverse className="text-center" style={{borderRadius: "20px"}}>
-              <CardImg top className="m-3" src={happy} alt="happy" height="256px"></CardImg>
-              <CardHeader className="layout-heading"> You have the power to bring happiness.</CardHeader>
-              <CardBody className="text">
-                <CardText>Many fake charity organizations pose as genuine and loot money from
-            innocent people in the name of charity. Our charity platform brings
-            transparency to online charity trusts.</CardText>
-              </CardBody>
-            </Card> 
-          </div>
-          <div className="col-12 col-sm-6">
-            <Card  color="primary" inverse className="text-center" style={{borderRadius: "20px"}}>
-              <CardImg top className="m-3" src={help} alt="help" height="256px"></CardImg>
-              <CardHeader className="layout-heading">Creating a Better Future through your Help </CardHeader>
-              <CardBody className="card-t">
-                <CardText>We have partnered with over 10 NGOs which have revolutionized
-                healthcare, sanitation, education and nutrition. We heartfully welcome NGO's to join us.</CardText>
-              </CardBody>
-            </Card>
-          </div>
+      <div className="row h-500 py-5 info-row">
+        <div className="col-12 col-sm-6">
+          <Card
+            color="success"
+            inverse
+            className="text-center"
+            style={{ borderRadius: "20px" }}
+          >
+            <CardImg
+              top
+              className="m-3"
+              src={happy}
+              alt="happy"
+              height="256px"
+            ></CardImg>
+            <CardHeader className="layout-heading">
+              {" "}
+              You have the power to bring happiness.
+            </CardHeader>
+            <CardBody className="text">
+              <CardText>
+                Many fake charity organizations pose as genuine and loot money
+                from innocent people in the name of charity. Our charity
+                platform brings transparency to online charity trusts.
+              </CardText>
+            </CardBody>
+          </Card>
         </div>
-        <div className="row mb-3 layout-row">
+        <div className="col-12 col-sm-6">
+          <Card
+            color="primary"
+            inverse
+            className="text-center"
+            style={{ borderRadius: "20px" }}
+          >
+            <CardImg
+              top
+              className="m-3"
+              src={help}
+              alt="help"
+              height="256px"
+            ></CardImg>
+            <CardHeader className="layout-heading">
+              Creating a Better Future through your Help{" "}
+            </CardHeader>
+            <CardBody className="card-t">
+              <CardText>
+                We have partnered with over 10 NGOs which have revolutionized
+                healthcare, sanitation, education and nutrition. We heartfully
+                welcome NGO's to join us.
+              </CardText>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+      <div className="row mb-3 layout-row">
         <div className="col-12 col-sm-4 p-2 h-auto">
-            <Layout
-              id="NGO"
-              title="Register yourself as a NGO"
-              content="To promote social or political change on a broad scale through our platform. "
-              image={ngo}
-              register = {async ()=>{
-                try {
-                  await registerNGO();
-                  alert("Transaction succesful!");
-                  routeChange('ngo');
-                } catch (err) {
-                  alert(err.message);
-                }  
-              }  
-              }  
-            />
-          </div>
-          <div className="col-12 col-sm-4 p-2">
-            <Layout
-              id="Donor"
-              title="Register yourself as a Donor"
-              content="Giving is not just make a donation, it's about making difference."
-              image={donors}
-              register = {async ()=>{
-                try {
-                  await registerDonor();
-                  alert("Transaction succesful!");
-                  routeChange('donor');
-                } catch (err) {
-                  alert(err.message);
-                }
-              }}
-            />
-          </div>
-          <div className="col-12 col-sm-4 p-2">
-            <Layout
-              id="Store"
-              title="Register yourself as a Store"
-              content="Help NGOs by providing your equipement at nominal prices."
-              image={store}
-              register = {async ()=>{
-                try {
-                  await registerStore();
-                  alert("Transaction succesful!");
-                  routeChange('store');
-                } catch (err) {
-                  alert(err.message);
-                }
-              }}
-            />
-          </div>  
+          <Layout
+            id="NGO"
+            title="Register yourself as a NGO"
+            content="To promote social or political change on a broad scale through our platform. "
+            image={ngo}
+            modalTitle="Register as NGO"
+            modal1="Name"
+            modal2="Registeration license"
+            modal3="Email"
+            register={async () => {
+              try {
+                await registerNGO();
+                alert("Transaction succesful!");
+                routeChange("ngo");
+              } catch (err) {
+                alert(err.message);
+              }
+            }}
+          />
         </div>
-  </div>
+        <div className="col-12 col-sm-4 p-2">
+          <Layout
+            id="Donor"
+            title="Register yourself as a Donor"
+            content="Giving is not just make a donation, it's about making difference."
+            image={donors}
+            modalTitle="Register as a Donor"
+            modal1="Name"
+            modal2="Aadhar Card"
+            modal3="Email"
+            register={async () => {
+              try {
+                await registerDonor();
+                alert("Transaction succesful!");
+                routeChange("donor");
+              } catch (err) {
+                alert(err.message);
+              }
+            }}
+          />
+        </div>
+        <div className="col-12 col-sm-4 p-2">
+          <Layout
+            id="Store"
+            title="Register yourself as a Store"
+            content="Help NGOs by providing your equipement at nominal prices."
+            image={store}
+            modalTitle="Register as a Store"
+            modal1="Name"
+            modal2="GST Number"
+            modal3="Email"
+            register={async () => {
+              try {
+                await registerStore();
+                alert("Transaction succesful!");
+                routeChange("store");
+              } catch (err) {
+                alert(err.message);
+              }
+            }}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
