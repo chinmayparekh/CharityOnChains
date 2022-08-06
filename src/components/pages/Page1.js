de44d3ebc6135006ee92ba330603f00ae6c09b09
chinmayparekh <chinmayparekh11@gmail.com>
2022-08-06 20:54:33 +0530

    fixed a bug in ui

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -12,22 +12,21 @@ const Page1 = (props) => {
   return (
     <>
       <div className="banner">
-         <div className="halfcontainer">
-            <img src={banner} alt="banner"/>
-         </div>
-         <div className="halfcontainer">
-            <div className="white mb-4">
-            Transform Your Business With <span className="cyan">BlockChain</span>
-            </div>
-            <div className="cyanline mb-4"></div>
-            <div className="white mb-5 smaller">
-            Disrupt your industry. Power your startup. Rationalize and control your business.
-            </div>
-           
-         </div>
+        <div className="halfcontainer">
+          <img src={banner} alt="banner" />
+        </div>
+        <div className="halfcontainer">
+          <div className="white mb-4">
+            Transform Your Business With{" "}
+            <span className="cyan">BlockChain</span>
+          </div>
+          <div className="cyanline mb-4"></div>
+          <div className="white mb-5 smaller">
+            Disrupt your industry. Power your startup. Rationalize and control
+            your business.
+          </div>
+        </div>
       </div>
-      <br></br>
-      <br></br>
     </>
   );
 };
