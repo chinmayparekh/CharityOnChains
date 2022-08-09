f78a6f81bcc6c8e4bb4e5b4e002159e7e9527dee
chinmayparekh <chinmayparekh11@gmail.com>
2022-08-09 23:40:44 +0530

    changed the smart contract

--- src/utils/tzkt.js
+++ src/utils/tzkt.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const fetchStorage = async () => {
   const res = await axios.get(
-    "https://api.jakartanet.tzkt.io/v1/contracts/KT1MT6xAxL4JvS7q9FTbNV186RABmLsZ5oo2/storage"
+    "https://api.jakartanet.tzkt.io/v1/contracts/KT1Q6hCw3q4RTaEoSrteCRf3sNJ8UvwriKQS/storage"
   );
   console.log(res.data);
   return res.data;
